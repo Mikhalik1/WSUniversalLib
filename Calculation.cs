5b5ea7e112133f35d52e58861b2bd493aa323166
rotoyouoi <rotoyouoi@yandex.ru>
2023-10-16 11:11:20 +0500

    Добавьте файлы проекта.

---
+++ Calculation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSUniversalLib
+{
+    public class Calculation
+    {
+        /// <summary>
+        /// Метод должен принимать идентификатор типа продукции, идентификатор типа материала, количество необходимой продукции для производства, ширину продукции и длину продукции
+        /// </summary>
+        /// <param name="productType"></param>
+        /// <param name="materialType"></param>
+        /// <param name="count"></param>
+        /// <param name="width"></param>
+        /// <param name="length"></param>
+        /// <returns>возвращать целое число - количество необходимого сырья с учетом возможного брака сырья.</returns>
+        public static int GetQuantityForProduct(int productType,int materialType,int count,float width,float length)
+        {
+            double mat = width * length;
+            double brak = 0;
+            switch (productType)
+            {
+                case 1:
+                    mat = mat * 1.1;
+                    break;
+                case 2:
+                    mat = mat * 2.5;
+                    break;
+                case 3:
+                    mat = mat * 8.43;
+                    break;
+                    
+            }
+            mat = mat * count;
+            switch (materialType)
+            {
+                case 1:
+                    brak = mat * 0.003;
+                    break;
+                case 2:
+                    brak = mat * 0.0012;
+                    break;
+            }
+
+            int matireal = Convert.ToInt32(Math.Ceiling(mat + brak));
+
+
+            return matireal;
+        }
+    }
+}
---
+++ WSUniversalLibtest/Calculationtest.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WSUniversalLib;
+using System.IO;
+
+namespace WSUniversalLibtest
+{
+    [TestClass]
+    public class Calculationtest
+    {
+        private void ReadInputData (string fileName,
+                                    out int productType,
+                                    out int materialType,
+                                    out int count,
+                                    out float width,
+                                    out float length)
+        {
+            string path = "..\\..\\..\\..\\TestingData\\"+ fileName;
+            string[] data =File.ReadAllLines(path);
+
+            productType = int.Parse(data[0]);
+            materialType= int.Parse(data[1]);
+            count = int.Parse(data[2]);
+            width = float.Parse(data[3]);
+            length = float.Parse(data[4]);
+        }
+
+        private int ReadOutputData(string fileName)
+        {
+            string path = "..\\..\\..\\..\\TestingData\\" + fileName;
+            string data = File.ReadAllText(path);
+            return int.Parse(data);
+        }
+        
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            int productType, materialType,count;
+            float width, length;
+            ReadInputData("InputData_Hard_10.txt",
+                                    out productType,
+                                    out materialType,
+                                    out count,
+                                    out width,
+                                    out length);
+            int expected = ReadOutputData("OutputData_Hard_10.txt");
+
+            int actual = Calculation.GetQuantityForProduct(
+                                                productType,
+                                                materialType,
+                                                count,
+                                                width,
+                                                length);
+
+            Assert.AreEqual(expected, actual);  
+        }
+    }
+}
